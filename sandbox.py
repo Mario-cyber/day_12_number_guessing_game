77696864dd1ccdc2d11ea4f5eb7c5f62f0b0f315
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-03 16:00:36 +0000

    trying to define a function for prime numbers

--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,15 @@
-fit 
+number = int(input("what number do you want to check: "))
+
+def is_prime(number):
+    if number == 1 :
+        return False
+    elif number == 2: 
+        return True
+    elif number.prime() :
+        return True    
+
+
+if is_prime(number):
+    print(f"the number {number} is prime")
+else: 
+    print(f"the number {number} is not prime")
