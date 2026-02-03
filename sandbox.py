cf5235a56e5d5d9682dd43a4471c4825c91e5dd8
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-03 17:10:30 +0000

    boy this is taking longet than I thgouth

--- sandbox.py
+++ sandbox.py
@@ -3,16 +3,19 @@ number = int(input("what number do you want to check: "))
 def is_prime(number):
     if number < 2 :
         return False
-    elif number == 2: 
+    if number == 2: 
         return True
-    elif number > 2:
-        for i in range(3, int(number ** 0.5 + 1)):
-            if number % i == 0 : 
-                return False 
+    if number % 2 == 0:
+        return False
 
+    for i in range(3,int(number ** 0.5 +1) ,2):
+        if number % 1 == 0:
+            return False
+    return True
+print(is_prime(number))
 
-for number in range(0,17,1):
-    print(f"the number i is prime is: {is_prime(number=number)}")
+# for number in range(0,17,1):
+#     print(f"the number i is prime is: {is_prime(number=number)}")
 
 # if is_prime(number):
 #     print(f"the number {number} is prime")
