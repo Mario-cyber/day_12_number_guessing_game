a93d4d3f6961e2acf2f3a8489353a711da4ad3ef
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-03 16:57:32 +0000

    prime function check

--- sandbox.py
+++ sandbox.py
@@ -1,15 +1,21 @@
 number = int(input("what number do you want to check: "))
 
 def is_prime(number):
-    if number == 1 :
+    if number < 2 :
         return False
     elif number == 2: 
         return True
-    elif number.prime() :
-        return True    
+    elif number > 2:
+        for i in range(3, int(number ** 0.5 + 1)):
+            if number % i == 0 : 
+                return False 
 
 
-if is_prime(number):
-    print(f"the number {number} is prime")
-else: 
-    print(f"the number {number} is not prime")
+for number in range(0,17,1):
+    print(f"the number i is prime is: {is_prime(number=number)}")
+
+# if is_prime(number):
+#     print(f"the number {number} is prime")
+# else: 
+#     print(f"the number {number} is not prime")
+
