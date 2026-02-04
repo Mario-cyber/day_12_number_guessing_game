03edc04f1d2db3865435c300014d0697585802df
Mario_F <54963398+Mario-cyber@users.noreply.github.com>
2026-02-04 19:54:31 +0000

    losing funcionality fixed

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -15,35 +15,36 @@ COMPUTER_GUESS = NUMBERS[random.randint(0,99)]
 
 print(COMPUTER_GUESS)
 
-COMPUTER_GUESS = 60
-
 #welcome interphase 
 
 print(f"Welcome to the number gessing game! \nI'm thinking of a number between 1 and 100")
-difficulty = input("Chose a difficulty, type 'easy' or 'hard: ").lower()
 
-print(difficulty)
+#set difficulty 
+difficulty = input("Chose a difficulty, type 'easy' or 'hard: ").lower()
 
 #define functions here:
 
 def guess_game():
+
     if difficulty == "hard":
         attemps = 5
     elif difficulty == "easy":
         attemps = 10 
     while attemps > 0 :
-        user_guess = int(input("enter your guess: "))
+        user_guess = int(input("make a guess: "))
         if user_guess == COMPUTER_GUESS:
             print("you win!")
-            attemps = 0
+            #this is a little cheeky, but it will do 
+            attemps = - 1
         else: 
             attemps = attemps - 1 
             if user_guess > COMPUTER_GUESS:
                 print("too high")
             else:
                 print("too low")
-            print(f"you have {attemps} attempts remiaing")
-
+            print(f"Guess again! \nyou have {attemps} attempts remiaing")
+    if attemps == 0:
+        print("you lose")
 
 guess_game()
 
